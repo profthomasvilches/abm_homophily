76adc71274e772d216e02d6c6efa284255eefa1f
profthomasvilches <thomas.vilches@unesp.br>
2025-06-25 09:00:55 -0300

    changes to match the other codes but keep old contact dynamics

---
+++ README.md
@@ -0,0 +1,10 @@
+# CODE 01
+
+This code tests if x infects y and y infects x
+
+popsize = 3000
+R0 = 2
+nprocs = 10
+nsims = 20
+
+time 80s
--- abm_behavior.jl
+++ abm_behavior.jl
@@ -31,7 +31,7 @@ Base.@kwdef mutable struct Human
     swap_status::HEALTH = UNDEF
     sickfrom::HEALTH = UNDEF
     sickby::Int64 = -1
-    nextday_meetcnt::Int16 = 0 ## how many contacts for a single day
+    nextday_meetcnt::UInt8 = 0 ## how many contacts for a single day
     age::Int16   = 0    # in years. don't really need this but left it incase needed later
     ag::Int16   = 0
     tis::Int16   = 0   # time in state 
@@ -40,7 +40,7 @@ Base.@kwdef mutable struct Human
     doi::Int16   = 999   # day of infection.
     iso::Bool = false  ## isolated (limited contacts)
     isovia::Symbol = :null ## isolated via quarantine (:qu), preiso (:pi), intervention measure (:im), or contact tracing (:ct)    
-    contacts_vac::Vector{Int64} = [0;0;0;0;0;0;0;0] # PRO NV; LIK; HES; ANTI; REC vac; DED vac; PRO VAC; VAC  
+    contacts_vac::Vector{UInt8} = UInt8.([0;0;0;0;0;0;0;0]) # PRO NV; LIK; HES; ANTI; REC vac; DED vac; PRO VAC; VAC  
     #comorbidity::Int8 = 0 ##does the individual has any comorbidity?
     # Taiye: We are not considering comorbidities at this stage.
 
@@ -61,9 +61,9 @@ Base.@kwdef mutable struct Human
     daysinf::Int64 = 999 
     isofalse::Bool = false
     
-    betas_vac::Vector{Float64} = [0.0; 0.0; 0.0; 0.0; 0.0] # bs, bh, bl, ba, be
+    betas_vac::Vector{Float32} = Float32.([0.0; 0.0; 0.0; 0.0; 0.0]) # bs, bh, bl, ba, be
    
-    totaldaysiso::Int32 = 0  
+    totaldaysiso::Int16 = 0  
 end
 
 ## default system parameters
@@ -129,17 +129,17 @@ function runsim(simnum, ip::ModelParameters)
 
     age_groups = [0:14, 15:24, 25:34, 35:44, 45:54, 55:64, 65:999]
     ags = map(x->findfirst(y-> x.age in y, age_groups),humans) # store a vector of the age group distribution 
-    spl = _splitstate(hmatrix, ags)
-    ag1 = _collectdf(spl[1])
-    ag2 = _collectdf(spl[2])
-    ag3 = _collectdf(spl[3])
-    ag4 = _collectdf(spl[4])
-    ag5 = _collectdf(spl[5])
-    ag6 = _collectdf(spl[6])
-    ag7 = _collectdf(spl[7])
-    insertcols!(all1, 1, :sim => simnum);insertcols!(allv, 1, :sim => simnum);  insertcols!(ag1, 1, :sim => simnum); insertcols!(ag2, 1, :sim => simnum); 
-    insertcols!(ag3, 1, :sim => simnum); insertcols!(ag4, 1, :sim => simnum); insertcols!(ag5, 1, :sim => simnum);
-    insertcols!(ag6, 1, :sim => simnum); insertcols!(ag7, 1, :sim => simnum);
+    #spl = _splitstate(hmatrix, ags)
+    # ag1 = _collectdf(spl[1])
+    # ag2 = _collectdf(spl[2])
+    # ag3 = _collectdf(spl[3])
+    # ag4 = _collectdf(spl[4])
+    # ag5 = _collectdf(spl[5])
+    # ag6 = _collectdf(spl[6])
+    # ag7 = _collectdf(spl[7])
+    insertcols!(all1, 1, :sim => simnum);insertcols!(allv, 1, :sim => simnum); # insertcols!(ag1, 1, :sim => simnum); insertcols!(ag2, 1, :sim => simnum); 
+    #insertcols!(ag3, 1, :sim => simnum); insertcols!(ag4, 1, :sim => simnum); insertcols!(ag5, 1, :sim => simnum);
+    #insertcols!(ag6, 1, :sim => simnum); insertcols!(ag7, 1, :sim => simnum);
     
 
     pos = findall(y-> y in (11,22,33),hmatrix[:,end])
@@ -151,7 +151,7 @@ function runsim(simnum, ip::ModelParameters)
         vector_ded[(x.age+1)] += 1
     end
 
-    return (a=all1, g1=ag1, g2=ag2, g3=ag3, g4=ag4, g5=ag5,g6=ag6,g7=ag7,allv = allv,
+    return (a=all1, allv = allv, #g1=ag1, g2=ag2, g3=ag3, g4=ag4, g5=ag5,g6=ag6,g7=ag7,
     vector_dead=vector_ded, R0 = R01, vac_number = vac_number)
 end
 export runsim
@@ -199,13 +199,13 @@ function main(ip::ModelParameters,sim::Int64)
         
         vac_number[st] = sum([x.vac_status for x in humans])
 
-        remaining_doses = vaccination(remaining_doses)
-        for x in humans
-    #        if x.iso && !(x.health_status in (HOS,ICU,DED)) # Taiye: Depends on whether we are considering HOS, ICU and DED.
-            if x.iso && !(x.health_status in (DED)) #&& !(x.health_status in (HOS,ICU,DED))
-                x.totaldaysiso += 1
-            end
-        end
+        remaining_doses = vaccination(remaining_doses, sim)
+        #     for x in humans
+        # #        if x.iso && !(x.health_status in (HOS,ICU,DED)) # Taiye: Depends on whether we are considering HOS, ICU and DED.
+        #         if x.iso && !(x.health_status in (DED)) #&& !(x.health_status in (HOS,ICU,DED))
+        #             x.totaldaysiso += 1
+        #         end
+        #     end
         _get_model_state(st, hmatrix) ## this datacollection needs to be at the start of the for loop
         _get_model_state2(st, vmatrix) ## this datacollection needs to be at the start of the for loop
         dyntrans(st, grps,sim)
@@ -221,16 +221,17 @@ function main(ip::ModelParameters,sim::Int64)
 end
 export main
 
-function vaccination(remaining_doses::Int64)
+function vaccination(remaining_doses::Int64, sim::Int64)
     if p.vaccination_rate == 0
         return 0
     end
+    rng = MersenneTwister(1234*sim)
     # pos = findall(x -> x.vac_status == 0 && x.vac_behavior ∈ (UNDEFV, PRO), humans)
     pos = findall(x -> x.vac_status == 0 && x.vac_behavior == PRO, humans)
 
     if length(pos) >= p.vaccination_rate+remaining_doses
 
-        pp = sample(pos,  p.vaccination_rate+remaining_doses, replace = false)
+        pp = sample(rng, pos,  p.vaccination_rate+remaining_doses, replace = false)
 
         for i in pp
             humans[i].vac_status = 1
@@ -400,8 +401,8 @@ function initialize()
     agedist = get_province_ag(p.prov)
     #agebraksnew = [0:4,5:14,15:24,25:34,35:44,45:54,55:64,65:74,75:84,85:99]
     
-    age_break_behav = [18:24,25:49,50:64,65:99]
-    prob_behav = [[0.38;0.214;0.205;0.088],[0.271;0.241;0.303;0.084],[0.322;0.302;0.223;0.053],[0.471;0.288;0.131;0.04]]
+    age_break_behav = [0:17, 18:24,25:49,50:64,65:99]
+    prob_behav = [[0.271;0.241;0.303;0.084],[0.38;0.214;0.205;0.088],[0.271;0.241;0.303;0.084],[0.322;0.302;0.223;0.053],[0.471;0.288;0.131;0.04]]
     a = [4;19;49;64;79;999]
      
     for i = 1:p.popsize 
@@ -417,7 +418,7 @@ function initialize()
         
         x.ag_new = g
         
-        if x.age >= 18
+        if x.age >= 0
             g = findfirst(y->x.age ∈ y, age_break_behav)
             x.vac_behavior = sample([PRO; LIK; HES; ANT], Weights(prob_behav[g]))
         end
@@ -435,11 +436,11 @@ function getting_b_values(p::ModelParameters)
     elseif p.b_value == :prob
 
         b1 = p.b
-        b2 = rand(Distributions.Beta(1.5, 20)) #ba
-        b3 = rand(Distributions.Beta(2, 25)) #bh
-        b4 = rand(Distributions.Beta(1, 200)) #bl
-        b5 = isnothing(p.probrec) ? rand(Distributions.Beta(2, 20)) : p.probrec #be
-        return [b1; b3; b4; b2./2; b5]./10
+        b2 = round(rand(Distributions.Beta(1.5, 20)), digits = 5) #ba
+        b3 = round(rand(Distributions.Beta(2, 25)), digits = 5) #bh
+        b4 = round(rand(Distributions.Beta(1, 200)), digits = 5) #bl
+        b5 = isnothing(p.probrec) ? round(rand(Distributions.Beta(2, 20)), digits = 5) : Float32(p.probrec) #be
+        return Float32.([b1; b3; b4; b2./2; b5]./10)
     else
         error("no strategy set for b values")
     end
@@ -822,8 +823,8 @@ export _get_betavalue
         x.nextday_meetcnt = cnt
     elseif !(x.health_status  in (DED))
         cnt = rand(negative_binomials_shelter(ag))  # expensive operation, try to optimize
-        x.nextday_meetcnt_w = 0
-        x.nextday_meetcnt = cnt
+        x.nextday_meetcnt = 0
+        x.nextday_meetcnt = 0
     end
     
 
@@ -831,7 +832,7 @@ export _get_betavalue
         x.nextday_meetcnt = 0
     end
    
-    x.contacts_vac = [0;0;0;0;0;0;0;0;0]
+    x.contacts_vac = UInt8.([0;0;0;0;0;0;0;0;0])
 
     return cnt
 end
@@ -873,10 +874,10 @@ function dyntrans(sys_time, grps,sim)
     for x in humans[pos]        
            
             xhealth = x.health
-            cnts = x.nextday_meetcnt
+            cnts = Int(x.nextday_meetcnt)
             cnts == 0 && continue # skip person if no contacts
             #general population contact
-            gpw = Int.(round.(cm[x.ag]*cnts))
+            gpw = Int.(round.(cm[x.ag].*cnts))
             
             #cnts = number of contacts on that day
 
@@ -1037,7 +1038,8 @@ function perform_contacts(x,gpw,grp_sample,xhealth, Pj)
             end  
         end
     end  
-
+    
+    x.nextday_meetcnt = 0
 end
 function contact_matrix()
     # regular contacts, just with 5 age groups. 
